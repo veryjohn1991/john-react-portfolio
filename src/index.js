d47b1c325503dba9a6f5a6bac91d119c5749526d
very john <armaanjelly@gmail.com>
2024-03-02 01:44:38 -0800

    debugging

--- src/index.js
+++ src/index.js
@@ -16,12 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-// Please, reference the README.md for further insight
-// HowToCreateReact.md and Resume.PDF are located in the documentation folder
-// Thank you for viewing!
-// Thomas Calle 😊
